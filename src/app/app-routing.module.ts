34764b39a255c95675d6d3655ca9cb8c473607bd
Arkadiusz Grzybowski <a.grzybowski@avsystem.com>
2022-05-19 18:42:16 +0200

    Added Stepper view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormComponent } from './components/form/form.component';
 import { HomeComponent } from './components/home/home.component';
+import { StepperComponent } from './components/stepper/stepper.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  { path: 'form', component: FormComponent }
+  { path: '', component: HomeComponent },
+  { path: 'form', component: FormComponent },
+  { path: 'stepper', component: StepperComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
 import { HomeComponent } from './components/home/home.component';
+import { StepperComponent } from './components/stepper/stepper.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialExampleModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
     HomeComponent,
+    StepperComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MaterialExampleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
